21712063db67f47ad51a1b43e3120d741c380b2f
GianluGiarde <gianlugiarde@gmail.com>
2023-10-18 16:11:50 +0200

    Add decimal format

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,10 +1,13 @@
 package org.lessons.java;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TrainTicket {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
         System.out.println("Quanti chilometri voi percorrere ?");
         int inputDistance = scan.nextInt();
@@ -13,7 +16,7 @@ public class TrainTicket {
         int inputYears = scan.nextInt();
 
         double basePrice = inputDistance * 0.21;
-        System.out.println("Prezzo base: " + basePrice + " €");
+        System.out.println("Prezzo base: " + decimalFormat.format(basePrice) + " €");
 
         double finalPrice;
 
@@ -25,7 +28,7 @@ public class TrainTicket {
             finalPrice = basePrice;
         }
 
-        System.out.print("Prezzo finale: " + finalPrice + " €");
+        System.out.print("Prezzo finale: " + decimalFormat.format(finalPrice) + " €");
 
         scan.close();
     }
